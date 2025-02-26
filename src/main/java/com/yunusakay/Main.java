2b8b720f7c82077a3c809d5185b9eb3ac260dd43
KravikKrokodil <yunusakay4072@gmail.com>
2025-02-26 18:38:11 +0300

    java core init

--- src/main/java/com/yunusakay/Main.java
+++ src/main/java/com/yunusakay/Main.java
@@ -2,7 +2,7 @@ package com.yunusakay;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-publica class Main {
+public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
